d10fc162682b013daeaec79b8a5ab0ea2c05b837
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-05-11 22:30:31 +0200

    Refactor:  Combined the reducers using combineReducers from Redux. Exported the combined reducers as default.

--- likor-web-app/src/store/reducer.js
+++ likor-web-app/src/store/reducer.js
@@ -1,53 +1,71 @@
-import { SET_USER, SET_CHANNEL, SET_FAVOURITECHANNEL, REMOVE_FAVOURITECHANNEL } from './actiontypes';
+import {
+  SET_USER,
+  SET_CHANNEL,
+  SET_FAVOURITECHANNEL,
+  REMOVE_FAVOURITECHANNEL,
+} from "./actiontypes";
 import { combineReducers } from "redux";
 
-let defaultUserState = {
-    currentUser: null
-}
+const defaultUserState = {
+  currentUser: null,
+};
 
 const userReducer = (state = defaultUserState, action) => {
-    if (action.type === SET_USER) {
-        let payload = action.payload;
-        state = { ...payload };
-        return state;
-    }
-    return state;
-}
+  switch (action.type) {
+    case SET_USER:
+      return {
+        ...state,
+        currentUser: action.payload.currentUser,
+      };
+    default:
+      return state;
+  }
+};
 
-let defaultChannelState = {
-    currentChannel: null,
-    loading : true
-}
-
+const defaultChannelState = {
+  currentChannel: null,
+  loading: true,
+};
 
 const channelReducer = (state = defaultChannelState, action) => {
-    if (action.type === SET_CHANNEL) {
-        let payload = action.payload;
-        state = { ...payload };
-        state.loading= false;
-        return state;
-    }
-    return state;
-}
+  switch (action.type) {
+    case SET_CHANNEL:
+      return {
+        ...state,
+        currentChannel: action.payload.currentChannel,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
 
-let defaultFavouriteChannelState = {
-    favouriteChannel: {}
-}
+const defaultFavouriteChannelState = {
+  favouriteChannel: {},
+};
 
+const favouriteChannelReducer = (
+  state = defaultFavouriteChannelState,
+  action
+) => {
+  const updatedState = { ...state.favouriteChannel };
+  switch (action.type) {
+    case SET_FAVOURITECHANNEL:
+      const { channelId, channelName } = action.payload.favouriteChannel;
+      updatedState[channelId] = channelName;
+      return { favouriteChannel: updatedState };
+    case REMOVE_FAVOURITECHANNEL:
+      delete updatedState[action.payload.favouriteChannel.channelId];
+      return { favouriteChannel: updatedState };
+    default:
+      return state;
+  }
+};
 
-const favouriteChannelReducer = (state = defaultFavouriteChannelState, action) => {
-    if (action.type === SET_FAVOURITECHANNEL) {
-        let payload = action.payload.favouriteChannel;
-        let updatedState = { ...state.favouriteChannel };
-        updatedState[payload.channelId] = payload.channelName;
-        return { favouriteChannel: updatedState };
-    } else if (action.type === REMOVE_FAVOURITECHANNEL) {
-        let payload = action.payload.favouriteChannel;
-        let updatedState = { ...state.favouriteChannel };
-        delete updatedState[payload.channelId];
-        return { favouriteChannel: updatedState };
-    }
-    return state;
-}
+const combinedReducers = combineReducers({
+  user: userReducer,
+  channel: channelReducer,
+  favouriteChannel: favouriteChannelReducer,
+});
 
-export const combinedReducers = combineReducers({ user: userReducer, channel: channelReducer ,favouriteChannel : favouriteChannelReducer  })
+export default combinedReducers;
