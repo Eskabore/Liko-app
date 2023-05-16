98d8deefdfa68572767a39f626143d652a943b24
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-05-16 23:29:27 +0200

    Err handling, File lookup  opt, +useEffect

--- likor-web-app/src/components/Messages/ImageUpload/ImageUpload.component.jsx
+++ likor-web-app/src/components/Messages/ImageUpload/ImageUpload.component.jsx
@@ -1,46 +1,60 @@
-import React, { useState } from 'react';
-import { Input, Modal, Button, Icon } from 'semantic-ui-react';
-import mime from "mime-types"
+import React, { useState, useEffect } from "react";
+import { Input, Modal, Button, Icon, Message } from "semantic-ui-react";
+import mime from "mime-types";
 
 export const ImageUpload = (props) => {
+  const [fileState, setFileState] = useState(null);
+  const [error, setError] = useState("");
 
-    const [fileState, setFileState] = useState(null);
+  const acceptedTypes = ["image/png", "image/jpeg"];
 
-    const acceptedTypes = ["image/png", "image/jpeg"]
-
-    const onFileAdded = (e) => {
-        const file = e.target.files[0];
-        if (file) {
-            setFileState(file);
-        }
+  useEffect(() => {
+    if (!props.open) {
+      setFileState(null);
+      setError("");
     }
+  }, [props.open]);
 
-    const submit = () => {
-        if (fileState && acceptedTypes.includes(mime.lookup(fileState.name))) {
-            props.uploadImage(fileState, mime.lookup(fileState.name));
-            props.onClose();
-            setFileState(null);
-        }
+  const onFileAdded = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileState(file);
     }
+  };
 
-    return (<Modal basic open={props.open} onClose={props.onClose}>
-        <Modal.Header>Select an image</Modal.Header>
-        <Modal.Content>
-            <Input
-                type="file"
-                name="file"
-                onChange={onFileAdded}
-                fluid
-                label="File Type (png , jpeg)"
-            />
-        </Modal.Content>
-        <Modal.Actions>
-            <Button color="blue" onClick={submit}>
-                <Icon name="checkmark" />Add
-            </Button>
-            <Button color="red" onClick={props.onClose}>
-                <Icon name="remove" />Cancel
-            </Button>
-        </Modal.Actions>
-    </Modal>)
-}
+  const submit = () => {
+    const mimeType = fileState ? mime.lookup(fileState.name) : null;
+    if (fileState && acceptedTypes.includes(mimeType)) {
+      props.uploadImage(fileState, mimeType);
+      props.onClose();
+    } else {
+      setError("Unsupported file type. Please upload png or jpeg file.");
+    }
+  };
+
+  return (
+    <Modal basic open={props.open} onClose={props.onClose}>
+      <Modal.Header>Select an image</Modal.Header>
+      <Modal.Content>
+        <Input
+          type='file'
+          name='file'
+          onChange={onFileAdded}
+          fluid
+          label='File Type (png , jpeg)'
+        />
+        {error && <Message error content={error} />}
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='blue' onClick={submit}>
+          <Icon name='checkmark' />
+          Add
+        </Button>
+        <Button color='red' onClick={props.onClose}>
+          <Icon name='remove' />
+          Cancel
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
